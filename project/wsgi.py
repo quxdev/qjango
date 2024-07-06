6f0a5023985f8c8c0da9d00042724c3b6a4efe64
vishal <vishal@qux.dev>
2024-07-06 06:15:22 -0400

    Refresh and support for python 3.12

--- project/wsgi.py
+++ project/wsgi.py
@@ -6,18 +6,23 @@ import dotenv
 from django.core.wsgi import get_wsgi_application
 
 
-def addpath(path):
-    if path not in sys.path:
-        print(f"--- PATH +{path}")
-        sys.path.insert(0, path)
-
-
 CURR_DIR = Path(__file__).resolve(strict=True).parent
+REPO_DIR = CURR_DIR.parent
+
+
+def addpath(path):
+    pathstr = path if hasattr(path, "lower") else str(path)
+    if pathstr not in sys.path:
+        print(f"addpath({pathstr})")
+        sys.path.insert(0, pathstr)
+
+
 dotenv.load_dotenv(Path(CURR_DIR, ".env"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-os.environ.setdefault("DJANGO_PYTHON_PATH", str(CURR_DIR.parent))
+os.environ.setdefault("DJANGO_PYTHON_PATH", str(REPO_DIR))
 
-addpath(CURR_DIR.parent)
+addpath(REPO_DIR)
 
 application = get_wsgi_application()
+
